1b3ac9811df3a5ae166b62e9292574130952168f
Joshua <3729344@myuwc.ac.za>
2022-04-29 12:13:52 +0200

    added to using-ts.ts

--- using-ts.js
+++ using-ts.js
@@ -1,6 +1,9 @@
-var num1 = "2";
-var num2 = "1";
-var add = function (n1, n2) {
-    return n1 + n2;
-};
-console.log(add(num1, num2));
+var button = document.querySelector("button");
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
+function add(num1, num2) {
+    return num1 + num2;
+}
+button.addEventListener("click", function () {
+    console.log(add(+input1.value, +input2.value));
+});
