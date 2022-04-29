adf4c52bbaba65392878865ec1e05321ffd91cea
Joshua <3729344@myuwc.ac.za>
2022-04-29 11:56:25 +0200

    first commit

---
+++ using-ts.js
@@ -0,0 +1,6 @@
+var num1 = "2";
+var num2 = "1";
+var add = function (n1, n2) {
+    return n1 + n2;
+};
+console.log(add(num1, num2));
---
+++ using-ts.ts
@@ -0,0 +1,8 @@
+const num1 = "2";
+const num2 = "1";
+
+const add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+console.log(add(num1, num2));
