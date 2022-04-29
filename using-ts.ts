8b534a31bcf9224b53171b61b73d50bc44b802e7
Joshua <3729344@myuwc.ac.za>
2022-04-29 12:05:45 +0200

    added using typescript script

--- using-ts.ts
+++ using-ts.ts
@@ -1,8 +1,11 @@
-const num1 = "2";
-const num2 = "1";
+const button = document.querySelector("button");
+const input1 = document.getElementById("num1")! as HTMLInputElement;
+const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-const add = (n1: number, n2: number) => {
-  return n1 + n2;
-};
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
 
-console.log(add(num1, num2));
+button.addEventListener("click", function () {
+  console.log(add(+input1.value, +input2.value));
+});
